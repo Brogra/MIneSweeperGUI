42abb9ac52c66f6844fb1e1a9ddbab7d26f6678e
HosniBerker <Hosni.berker@gmail.com>
2018-01-24 14:04:44 -0500

    Text Base Code
    Added Code for text base version

---
+++ src/Square.java
@@ -0,0 +1,97 @@
+public class Square {
+    private int Touching;
+    private int xLoc;
+    private int yLoc;
+    private boolean Hidden;
+    private boolean Clicked;
+    private boolean isMine;
+    private boolean Test;
+    public Square(int MinesTouching, int xCord, int yCord, boolean hide,boolean click,boolean mine,boolean tested){
+        Touching = MinesTouching;
+        xLoc = xCord;
+        yLoc = yCord;
+        Hidden = hide;
+        Clicked = click;
+        isMine = mine;
+        Test = tested;
+    }
+    public void setTest(boolean set){
+        Test = set;
+    }
+    public boolean didTest(){
+        return Test;
+    }
+    public boolean isHidden(){
+        return Hidden;
+    }
+
+    public void firstClick(){
+        isMine = false;
+
+    }
+    public void click(){
+        Clicked = true;
+        Hidden = false;
+    }
+    public int[] getXAndY(){
+        int[] loc = {xLoc,yLoc};
+        return loc;
+    }
+    public int[] info(){
+        int[] info = {xLoc,yLoc,Touching};
+        Hidden = false;
+        if(isMine){
+            info[0] = 42;
+        }
+        return info;
+    }
+    public boolean isMine(){
+        return isMine;
+    }
+    public boolean notHidden(){
+        if(!Hidden){
+            return  true;
+        }
+        return false;
+    }
+    public void setMine(){
+        isMine = true;
+    }
+    public void revealSpot(){
+            Hidden = false;
+
+    }
+    public void updateTouch(int value){
+        Touching = value;
+    }
+    public boolean didClick(){
+        return Clicked;
+    }
+    public int intTouching(){
+        return Touching;
+    }
+    public String touching(){
+        return String.valueOf(Touching);
+    }
+    public String icon() {
+        if (!Clicked){
+            return "#";
+        }
+        if (Clicked && !isMine) {
+            return String.valueOf(Touching);
+        }else if(isMine && Clicked){
+            return "+";
+        }else if(!Hidden){
+            return String.valueOf(Touching);
+        }
+        return "#";
+    }
+    public void setXCord(int x){
+        xLoc = x;
+    }
+    public void setYCord(int y){
+        yLoc = y;
+    }
+
+
+}
---
+++ src/main.java
@@ -0,0 +1,355 @@
+import java.util.*;
+
+public class main {
+    final static int SIZE = 8;
+    final static int BEGMIN = 5;
+    static Random rand = new Random();
+    static Scanner read = new Scanner(System.in);
+    static boolean firstClick = true;
+
+
+
+    public static ArrayList<Square> setMines(ArrayList<int[]> locs, ArrayList<Square> squaresObj){
+        System.out.println("     " + locs.get(0)[0] + " " +locs.get(0)[1]);
+        System.out.println(locs.get(1)[0] + " " +locs.get(1)[1]);
+        for (int x = 0 ; x < locs.size();x++){
+            for (int i = 0; i < squaresObj.size(); i++) {
+                if(Arrays.equals(squaresObj.get(i).getXAndY(),(locs.get(x)))){
+                    squaresObj.get(i).setMine();
+                }
+            }
+        }
+        return squaresObj;
+    }
+
+    public static ArrayList<int[]> mineLoc(){
+        ArrayList<int[]> Locations = new ArrayList<>();
+
+        int[] place = new int[2];
+        int x;
+        int[] clone;
+
+        for (int e = 0; e < BEGMIN; e++) {
+
+            clone = place.clone();
+            x = rand.nextInt(SIZE)+ 1;
+            clone[0] = x;
+            x = rand.nextInt(SIZE)+ 1;
+            clone[1] = x;
+            Locations.add(e,clone);
+        }
+        for (int[] pair: Locations) {
+            System.out.println("ML: " + pair[0] + " " + pair[1]);
+        }
+        return Locations;
+    }
+
+    public static void Draw(ArrayList<Square> squaresObj){
+        for (int i = 0; i < SIZE * 2 + 1; i++) {
+            System.out.print(" ");
+
+        }
+        System.out.println("X Col");
+        System.out.println("        ___________");
+        System.out.print("         ");
+
+
+        for (int i = 1; i < SIZE + 1; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        int place = 0;
+        for(int i = 1; i < SIZE + 1;i++){
+            System.out.print("Y Row " + i + "[ ");
+            for (int e = 1; e < SIZE + 1; e++) {
+                squaresObj.get(place).setXCord(e);
+                squaresObj.get(place).setYCord(i);
+
+                System.out.print(squaresObj.get(place).icon()+ " ");
+                place++;
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static ArrayList<Square> createBoxes(){
+        ArrayList<Square> squares = new ArrayList<Square>();
+        //String[] names = new String[SIZE*SIZE];
+        for (int d = 0; d < SIZE*SIZE;d++) {
+            Square a = new Square(0,0,0, true,false,false,false);
+            squares.add(a);
+        }
+
+        int p = 0;
+        for (int i = 1; i < (SIZE) + 1 ; i++) {
+            for (int e = 1; e < SIZE + 1; e++) {
+                squares.get(p).setXCord(e);
+                squares.get(p).setYCord(i);
+                p++;
+            }
+        }
+        return squares;
+    }
+
+    public static int[] getMove(){
+        System.out.println();
+        System.out.println("Enter Square Cords in form \"xCord yCord\"");
+        int[] cords = new int[2];
+        cords[0] = read.nextInt();
+        cords[1] = read.nextInt();
+        return cords;
+    }
+
+    public static boolean WON(ArrayList<Square> squaresObj){
+        int total = 0;
+        for (Square x:squaresObj) {
+            if(x.isMine() && !x.isHidden()){
+                return true;
+            }
+            if(!x.isHidden() && x.notHidden()){
+                total++;
+            }
+        }
+        if (total == (SIZE * SIZE) - BEGMIN) {
+            return true;
+        }
+        return false;
+    }
+
+    public static ArrayList<Square> update(int[] cords, ArrayList<Square> squaresObj) {
+        main run = new main();
+        //squaresObj.get(findIndex(cords,squaresObj)).click();
+        ArrayList<Square> clone = new ArrayList<Square>();
+        clone = (ArrayList<Square>) squaresObj.clone();
+        if(cords[0] < 1 || cords[0] > SIZE || cords[1] < 1 || cords[1] > SIZE){
+            return clone;
+        }
+        if(clone.get(findIndex(cords,clone)).intTouching() == 0 && clone.get(findIndex(cords,clone)).isHidden()){
+
+
+            clone.get(findIndex(cords,clone)).click();
+            squaresObj = clone;
+            ArrayList<int[]> soourond = new ArrayList<>();
+            soourond = getSurrounding(cords);
+            update(soourond.get(0), clone);
+            update(soourond.get(1),clone);
+            update(soourond.get(2),clone);
+            update(soourond.get(3),clone);
+            update(soourond.get(4),clone);
+            update(soourond.get(5),clone);
+            update(soourond.get(6),clone);
+            update(soourond.get(7),clone);
+
+
+        }else{
+            if(clone.get(findIndex(cords,clone)).icon().equals("+")){
+
+            }else{
+                clone.get(findIndex(cords,clone)).click();
+            }
+
+            return clone;
+        }
+        return clone;
+    }
+
+    public static int istouch(int[] cords, ArrayList<Square> obj) {
+        int touching = 0;
+        if (cords[0] + 1 <= SIZE) {
+            int[] newCord = {cords[0] + 1, cords[1]};
+            for (Square spot : obj) {
+                if (Arrays.equals(spot.getXAndY(), (newCord))) {
+                    if (spot.isMine()) {
+                        touching++;
+                    }
+
+                }
+
+            }
+        }
+        if (cords[0] - 1 > 0) {
+            int[] newCord = {cords[0] - 1, cords[1]};
+            for (Square spot : obj) {
+                if (Arrays.equals(spot.getXAndY(), (newCord))) {
+                    if (spot.isMine()) {
+                        touching++;
+                    }
+
+
+                }
+
+            }
+        }
+
+        if (cords[1] - 1 > 0) {
+            int[] newCord = {cords[0], cords[1] - 1};
+            for (Square spot : obj) {
+                if (Arrays.equals(spot.getXAndY(), (newCord))) {
+                    if (spot.isMine()) {
+                        touching++;
+                    }
+                }
+            }
+        }
+
+        if (cords[1] + 1 > 0) {
+            int[] newCord = {cords[0], cords[1] + 1};
+            for (Square spot : obj) {
+                if (Arrays.equals(spot.getXAndY(), (newCord))) {
+                    if (spot.isMine()) {
+                        touching++;
+                    }
+                }
+            }
+        }
+
+        if (cords[1] - 1 > 0) {
+            int[] newCord = {cords[0] - 1, cords[1] - 1};
+            for (Square spot : obj) {
+                if (Arrays.equals(spot.getXAndY(), (newCord))) {
+                    if (spot.isMine()) {
+                        touching++;
+                    }
+                }
+            }
+        }
+
+        if (cords[1] - 1 > 0) {
+            int[] newCord = {cords[0] - 1, cords[1] + 1};
+            for (Square spot : obj) {
+                if (Arrays.equals(spot.getXAndY(), (newCord))) {
+                    if (spot.isMine()) {
+                        touching++;
+                    }
+                }
+            }
+        }
+
+        if (cords[1] - 1 > 0) {
+            int[] newCord = {cords[0] + 1, cords[1] + 1};
+            for (Square spot : obj) {
+                if (Arrays.equals(spot.getXAndY(), (newCord))) {
+                    if (spot.isMine()) {
+                        touching++;
+                    }
+                }
+            }
+        }
+
+        if (cords[1] - 1 > 0) {
+            int[] newCord = {cords[0] + 1, cords[1] - 1};
+            for (Square spot : obj) {
+                if (Arrays.equals(spot.getXAndY(), (newCord))) {
+                    if (spot.isMine()) {
+                        touching++;
+                    }
+                }
+            }
+        }
+        return touching;
+    }
+
+    public static  ArrayList<Square> touching(Square y, ArrayList<Square> obj){
+        int[] cords = y.getXAndY();
+        int spot = 0;
+        for (Square x:obj) {
+            if (x.equals(y)){
+                x.updateTouch(istouch(x.getXAndY(),obj));
+            }
+            spot++;
+        }
+        return obj;
+
+    }
+
+    public static boolean didLose(ArrayList<Square> squaresObj){
+        for (Square x:squaresObj) {
+            if(x.isMine() && x.notHidden()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int findIndex(int[] cords, ArrayList<Square> obj){
+        int place  = 0;
+        for(Square i : obj){
+            if (Arrays.equals(i.getXAndY(), cords)){
+
+                return place;
+            }
+            place++;
+        }
+        return place;
+    }
+
+    public static  ArrayList<int[]> getSurrounding(int[] cords){
+        int[] s1 = cords;
+        int[] s2 = {cords[0] - 1, cords[1] - 1};
+        int[] s3 = {cords[0], cords[1] - 1};
+        int[] s4 = {cords[0] + 1, cords[1] - 1};
+        int[] s5 = {cords[0] - 1, cords[1]};
+        int[] s6 = {cords[0] + 1, cords[1]};
+        int[] s7 = {cords[0] - 1, cords[1] + 1};
+        int[] s8 = {cords[0], cords[1]  + 1};
+        int[] s9 = {cords[0] + 1 , cords[1] + 1};
+        ArrayList<int[]> gone= new ArrayList<>();
+        gone.add(s1);
+        gone.add(s2);
+        gone.add(s3);
+        gone.add(s4);
+        gone.add(s5);
+        gone.add(s6);
+        gone.add(s7);
+        gone.add(s8);
+        gone.add(s9);
+        return gone;
+
+    }
+
+    public static ArrayList<Square> setvalues(ArrayList<Square> objts){
+        for (int i = 0; i <objts.size() ; i++) {
+            objts = touching(objts.get(i),objts);
+        }
+        return objts;
+    }
+
+    public ArrayList<Square> firstClick(int[] cord, ArrayList<Square> obj){
+        if(firstClick){
+            obj.get(findIndex(cord,obj)).firstClick();
+            firstClick = false;
+        }
+        return obj;
+    }
+    public static void main(String args[]){
+        int[] test = {9,9};
+
+        ArrayList<Square> squaresObj = createBoxes();
+        boolean won = false;
+
+        ArrayList<int[]> mines = mineLoc();
+        squaresObj = setMines(mines,squaresObj);
+        squaresObj = setvalues(squaresObj);
+
+        int[] cords;
+        while(!won){
+            Draw(squaresObj);
+            cords = getMove();
+            update(cords,squaresObj);
+            //won = didLose(squaresObj);
+            won = WON(squaresObj);
+        }
+
+        Draw(squaresObj);
+        if(didLose(squaresObj)){
+            System.out.println("You Lost");
+
+        }else{
+            System.out.println("You Won");
+
+        }
+
+
+    }
+}
